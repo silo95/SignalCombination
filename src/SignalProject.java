97a49f4c9917548702eed2bf4137787d6b9132d9
silo_ <silo_@DESKTOP-7K4RMVO>
2019-09-03 11:46:33 +0200

    Spostate le dichiarazioni delle variabili fuori dal for

--- src/SignalProject.java
+++ src/SignalProject.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.*;
 import java.util.concurrent.locks.*;
 
 public class SignalProject {
-    public final static int NUM_THREADS = 8;
+    public final static int NUM_THREADS = 20;
     private final static ExecutorService myExecutor = 
             Executors.newFixedThreadPool(NUM_THREADS);
     private SignalSample sigSample;
@@ -15,10 +15,10 @@ public class SignalProject {
     public static final double signalFrequency = 10;
     public static final double signalAmplitude = 2;
     public static final double signalPhase = 0;
-    public static final double lossPercentage = 0.2;
+    public static final double lossPercentage = 0.3;
     public static final double noiseMean = 0.0;
     public static final double noiseVariance = 1.0;
-    public final static double MAX_SAMPLE_TIME = 1.0;
+    public final static double MAX_SAMPLE_TIME = 100.0;
     public final static double samplingTime = 0.001;
     public final static Lock sharedLock = new ReentrantLock();
     public final static Condition finished = sharedLock.newCondition();
@@ -31,12 +31,16 @@ public class SignalProject {
         resPrinter = new ResultPrinter(sharedLock, finished, resultQueue,
                 writtenResults, queueCapacity);
         SignalProject myInst;
+        AtomicInteger counterSamples;
+        Lock myLock;
+        Condition cond;
+        LinkedList<Sample> myQueue;
         for(double i = 0; i<=MAX_SAMPLE_TIME; i+=samplingTime){
             myInst = new SignalProject();
-            AtomicInteger counterSamples = new AtomicInteger(0);
-            Lock myLock = new ReentrantLock();
-            Condition cond = myLock.newCondition();
-            LinkedList<Sample> myQueue = new LinkedList<Sample>();
+            counterSamples = new AtomicInteger(0);
+            myLock = new ReentrantLock();
+            cond = myLock.newCondition();
+            myQueue = new LinkedList<Sample>();
             myInst.sigSample = new SignalSample(myLock, cond, i, myQueue,
                     counterSamples, signalAmplitude, signalFrequency, signalPhase);
             myInst.addNoise = new AdditiveNoise(myLock, cond, i, myQueue,
